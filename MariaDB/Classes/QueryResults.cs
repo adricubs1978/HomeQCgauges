afff58387aea8bf089bb91f78b0ca660c779b1ab
adricubs1978 <99996218+adricubs1978@users.noreply.github.com>
2022-04-18 10:29:21 +0100

    Read from DB 2DResults method done
    Method to read from DB done, not tested#3

---
+++ MariaDB/Classes/QueryResults.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MariaDB.Classes
+{
+    public class QueryResults
+    {
+        public List<string> ColumnNames { get; set; }
+        public List<List<string>> Results2D { get; set; }
+    }
+}
--- MariaDB/MariaDB.cs
+++ MariaDB/MariaDB.cs
@@ -1,5 +1,8 @@
 ﻿using MySqlConnector;
 using System;
+using MariaDB.Classes;
+using System.Collections.Generic;
+
 
 namespace MariaDB
 {
@@ -10,8 +13,9 @@ namespace MariaDB
         private string _user;
         private string _password;
         private string _database;
+        private MySqlConnection _connection;
 
-        
+
         public MariaDB(string IP, UInt32 Port, String User, String Password, String Database)
         {
             // Creating the instance should provide all the required info for connecting, and open and close connection + test connection
@@ -22,7 +26,8 @@ namespace MariaDB
             _database = Database;
 
             string _connection_str = $"datasource={_IP};port={_port.ToString()};username={_user};pwd={_password};database={_database}";
-            MySqlConnection _connection = new MySqlConnection(_connection_str);
+
+            _connection = new MySqlConnection(_connection_str);
             _connection.Open();
             if (_connection.Ping())
             {
@@ -34,8 +39,48 @@ namespace MariaDB
             }
                 ;
             _connection.Close();
+          
+        }
+        public QueryResults ReadQuery(string Query,bool AllowEmpty)
+        {
+            QueryResults QueryResults = new QueryResults();
+            List<List<string>> Results2D = new List<List<string>>();
+            
 
+            MySqlCommand ReadQueryCommand = new MySqlCommand(Query, _connection);
+            MySqlDataReader dataread = ReadQueryCommand.ExecuteReader();
+            if (dataread.HasRows! && AllowEmpty!)
+            {
+                throw new Exception($"Query '{Query}' did not give any results back");
+            } else
+            {
+                //Read column names
+                // Read full row
+                for (int i = 0; i < dataread.FieldCount; i++)
+                {
+                    //Read all column names
+                    QueryResults.ColumnNames.Add(dataread.GetName(i));
+                }
 
+                while (dataread.Read())
+                {
+                    // Read full row
+
+                    List<string> RowData=new();//This is the list inside Results2D
+
+                    for (int i = 0; i < dataread.FieldCount; i++)
+                    {
+                        //Read all columns in the row
+                        RowData.Add(dataread.GetString(i));
+                        
+                    }
+                    QueryResults.Results2D.Add(RowData);
+                    RowData.Clear();
+             
+                }
+            }
+          
+            return QueryResults;
         }
     }
 }
